174c40e14452cc03823cf8a8d97f90210a4b865c
Aditya Choudhary <tejash835274@gmail.com>
2025-03-29 05:10:04 +0530

    oo

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -28,7 +28,7 @@ const googleLogin = async (req, res) => {
     try {
         const { code } = req.query;
         const googleRes = await oauth2client.getToken(code);
-        oauth2client.setCredentials(googleRes.tokens);
+        await oauth2client.setCredentials(googleRes.tokens);
 
         const userRes = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`, { method: 'GET' });
 
